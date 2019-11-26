9aa5ad4e6a269e8c3456a7db360e009dfea75ab7
Jayant Porwal <jayantporwal05@gmail.com>
2019-11-26 21:50:56 +0530

    Updated comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## makeCacheMatrix creates a matrix that can be cached
 
 ## cacheSolve computes the inverse of the matrix pointed by 
-## the list of operations created by makeCacheMatrix
+## the list of operations created by makeCacheMatrix and caches it
+## for further calls to this function using same matrix
 
 ## This function takes a matrix as an input and returns
 ## a list of operations that point to the input matrix
@@ -22,6 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function takes a list of operations as an input
 ## This list points to a matrix whose inverse needs to be computed
+## If the inverse is already computed it will return from cache
+## If the inverse is being computed for the first time, it will
+## cache it after computation and then return it.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
